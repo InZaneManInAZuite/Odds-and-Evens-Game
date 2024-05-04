2d5c0f1f4c4e9cfeb15cea6125a88a1d75b128fc
Toshiro <toshiro.mendoza@gmail.com>
2024-05-05 02:05:36 +1200

    Displays to the user its name and the finger picked, completed task 1

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -7,31 +7,40 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   private int round = 0;
+  private String player;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
+
     // the first element of options[0]; is the name of the player
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    player = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(player);
   }
 
   public void play() {
+
+    // The round is incremented and the player is informed
     round++;
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
 
     // The player is asked to input the number of fingers
+    MessageCli.ASK_INPUT.printMessage();
+
+    // The input is obtained from the player
     boolean inputIsValid = false;
+    String input = "";
     while (!inputIsValid) {
-      MessageCli.ASK_INPUT.printMessage();
-      // The player inputs the number of fingers
-      String input = Utils.scanner.nextLine();
+
+      input = Utils.scanner.nextLine();
+
       // The input is validated
       if (input.matches("[0-5]")) {
         inputIsValid = true;
       } else {
         MessageCli.INVALID_INPUT.printMessage();
       }
-      // If the input is invalid, the player is asked to input again
-      // If the input is valid, the game continues
     }
+
+    MessageCli.PRINT_INFO_HAND.printMessage(player, input);
   }
 
   public void endGame() {}
